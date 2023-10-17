44b7acc6cee29a76ae166af48303ec1715492539
Abdellah Aarab <105945475+abdellahaarab@users.noreply.github.com>
2023-10-17 11:00:42 +0100

    Add les conditon file
    Add les conditon file

---
+++ CoursCpp/lesConditon.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+	int a = 0;
+	a += 1 ;
+	printf("a = %d",a);
+	
+	return 0;
+}
+
+//int main(){
+//	int a= 26,b=18;
+//	printf("%d" , a<b);
+
+ 
+//	if(a<b){
+//		printf("%d est petit \n",a);
+//	}
+//	else{
+//		 printf("%d est grand \n",a);	
+//	}
+//	
+
+//	int c = (a<b) ? a : b;
+//	printf("%d est prtit \n", c);
+	
+//	int a = 0;
+//	int b,c,d;
+//	printf("%d \n" , a);
+//	a=a+1;
+//	printf("%d \n", a);
+//	b=++a;
+//	printf("valeur de A est : %d valeure de b est: %d\n", a,b);
+//	
+//	c=(a++) + (++b);
+//	printf("valeur de A est : %d valeure de b est: %d valeure de c est: %d \n", a,b,c);
+//	
+//	d=(++a) + (++c) + (++a);
+//	printf("valeur de A est : %d valeure de b est: %d valeure de c est: %d valeure de d est: %d \n", a,b,c,d);
+	
+//	return 0;
+//})
+
